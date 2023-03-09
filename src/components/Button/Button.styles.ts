36bd00a533d7cda503ce26df65c07eb956089107
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-09 21:36:15 +0900

    add eslint prettier

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,39 +1,39 @@
-import { ButtonProps } from "./Button.types";
+import { ButtonProps } from './Button.types'
 
-export const getColor = (colorKey: ButtonProps["color"]) =>
-  new Map<ButtonProps["color"], string>([
-    ["none", ""],
-    ["primary", "text-white bg-primary hover:bg-primary_hover"],
-    ["danger", "text-white bg-danger hover:bg-danger_hover"],
-    ["success", "text-white bg-success hover:bg-success_hover"],
-  ]).get(colorKey);
+export const getColor = (colorKey: ButtonProps['color']) =>
+  new Map<ButtonProps['color'], string>([
+    ['none', ''],
+    ['primary', 'text-white bg-primary hover:bg-primary_hover'],
+    ['danger', 'text-white bg-danger hover:bg-danger_hover'],
+    ['success', 'text-white bg-success hover:bg-success_hover'],
+  ]).get(colorKey)
 
-export const getSize = (sizeKey: ButtonProps["size"]) =>
-  new Map<ButtonProps["size"], string>([
-    ["sm", "w-32 px-3 py-2 block"],
-    ["md", "w-60 px-4 py-3 block"],
-    ["full", "w-full px-5 py-4 block"],
-  ]).get(sizeKey);
+export const getSize = (sizeKey: ButtonProps['size']) =>
+  new Map<ButtonProps['size'], string>([
+    ['sm', 'w-32 px-3 py-2 block'],
+    ['md', 'w-60 px-4 py-3 block'],
+    ['full', 'w-full px-5 py-4 block'],
+  ]).get(sizeKey)
 
-export const getShape = (shapeKey: ButtonProps["shape"]) =>
-  new Map<ButtonProps["shape"], string>([
-    ["default", "rounded"],
-    ["rounded-md", "rounded-md"],
-    ["rounded-lg", "rounded-lg"],
-    ["rounded-full", "rounded-full"],
-  ]).get(shapeKey);
+export const getShape = (shapeKey: ButtonProps['shape']) =>
+  new Map<ButtonProps['shape'], string>([
+    ['default', 'rounded'],
+    ['rounded-md', 'rounded-md'],
+    ['rounded-lg', 'rounded-lg'],
+    ['rounded-full', 'rounded-full'],
+  ]).get(shapeKey)
 
-export const getTextSize = (textSizeKey: ButtonProps["textSize"]) =>
-  new Map<ButtonProps["textSize"], string>([
-    ["sm", "text-sm"],
-    ["md", "text-base"],
-    ["lg", "text-lg"],
-  ]).get(textSizeKey);
+export const getTextSize = (textSizeKey: ButtonProps['textSize']) =>
+  new Map<ButtonProps['textSize'], string>([
+    ['sm', 'text-sm'],
+    ['md', 'text-base'],
+    ['lg', 'text-lg'],
+  ]).get(textSizeKey)
 
-export const getOutline = (outlineKey: ButtonProps["outline"]) =>
-  new Map<ButtonProps["outline"], string>([
-    ["none", ""],
-    ["outline-primary", "outline outline-2 outline-primary"],
-    ["outline-success", "outline outline-2 outline-success"],
-    ["outline-danger", "outline outline-2 outline-danger"],
-  ]).get(outlineKey);
+export const getOutline = (outlineKey: ButtonProps['outline']) =>
+  new Map<ButtonProps['outline'], string>([
+    ['none', ''],
+    ['outline-primary', 'outline outline-2 outline-primary'],
+    ['outline-success', 'outline outline-2 outline-success'],
+    ['outline-danger', 'outline outline-2 outline-danger'],
+  ]).get(outlineKey)
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,34 +1,47 @@
-import React  from "react";
-import { ButtonProps } from "./Button.types";
-import { getColor, getOutline, getShape, getSize, getTextSize } from "./Button.styles";
+import React from 'react'
+import { ButtonProps } from './Button.types'
+import {
+  getColor,
+  getOutline,
+  getShape,
+  getSize,
+  getTextSize,
+} from './Button.styles'
 
-const Button = ({ color, text, size, shape, textSize, outline, suffix, ...props}: ButtonProps) => {
+const Button = ({
+  color,
+  text,
+  size,
+  shape,
+  textSize,
+  outline,
+  suffix,
+  ...props
+}: ButtonProps) => {
   const colorStyle = getColor(color)
   const sizeStyle = getSize(size)
   const shapeStyle = getShape(shape)
   const textSizeStyle = getTextSize(textSize)
   const outlineStyle = getOutline(outline)
-  const suffixStyle = "flex items-center justify-center"
-  
-  return (
-    suffix ? (
-      <button
-        className={`${colorStyle} ${sizeStyle} ${shapeStyle} ${textSizeStyle} ${outlineStyle} ${suffixStyle}`}
-        {...props}
-      >
-        <span>{text}</span>
-        <span className="ml-1">{suffix}</span>
-      </button>
-    ) : (
-      <button
-        className={`${colorStyle} ${sizeStyle} ${shapeStyle} ${textSizeStyle} ${outlineStyle} ${suffixStyle}`}
-        {...props}
-      >
-        {text}
-      </button>
-    )
-  );
-};
+  const suffixStyle = 'flex items-center justify-center'
 
-export default Button;
-export type { ButtonProps };
+  return suffix ? (
+    <button
+      className={`${colorStyle} ${sizeStyle} ${shapeStyle} ${textSizeStyle} ${outlineStyle} ${suffixStyle}`}
+      {...props}
+    >
+      <span>{text}</span>
+      <span className="ml-1">{suffix}</span>
+    </button>
+  ) : (
+    <button
+      className={`${colorStyle} ${sizeStyle} ${shapeStyle} ${textSizeStyle} ${outlineStyle} ${suffixStyle}`}
+      {...props}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button
+export type { ButtonProps }
