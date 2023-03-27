1cd5ed484f6db2d1d716e3216a49f1529c34c5c9
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-28 02:51:33 +0900

    display several types of button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -15,25 +15,45 @@ export default {
   },
 } as ComponentMeta<typeof Button>
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+export const _Button: ComponentStory<typeof Button> = (args) => 
+  <>
+  <div className='flex gap-4'>
+    <Button {...args} text='Button' color='primary' size='md' textSize='md' />
+    <Button {...args} text='Button' color='primary' size='md' textSize='md' shape='rounded-full' />
+    <Button {...args} text='Button' color='none' size='md' textSize='md' shape='default' outline='outline-primary' />
+  </div>
+  <br />
+    <Button {...args} text='Button' color='danger' size='sm' shape='default' textSize='sm' />
+    <br />
+    <Button {...args}  text='Button' color='danger' size='md' shape='default' textSize='md' />
+    <br />
+    <Button {...args} text='Button' color='danger' size='full' shape='rounded-lg' textSize='lg' />
+    <br/>
+    <Button {...args}  text='Button' color='success' size='sm' shape='default' textSize='sm' suffix={<FaPlusCircle />} />
+    <br/>
+    <Button {...args}  text='Button' color='success' size='sm' shape='rounded-full' textSize='sm' suffix={<FaPlusCircle />} />
+  </>
 
-export const HelloWorld = Template.bind({})
-HelloWorld.args = {
-  text: 'Hello World',
-  color: 'primary',
-  size: 'md',
-  shape: 'default',
-  textSize: 'md',
-  outline: 'none',
-  suffix: <FaPlusCircle />,
-}
 
-export const ClickMe = Template.bind({})
-ClickMe.args = {
-  text: 'Click Me',
-  color: 'primary',
-  size: 'md',
-  shape: 'default',
-  textSize: 'md',
-  outline: 'none',
-}
+// const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+
+// export const HelloWorld = Template.bind({})
+// HelloWorld.args = {
+//   text: 'Hello World',
+//   color: 'primary',
+//   size: 'md',
+//   shape: 'default',
+//   textSize: 'md',
+//   outline: 'none',
+//   suffix: <FaPlusCircle />,
+// }
+
+// export const ClickMe = Template.bind({})
+// ClickMe.args = {
+//   text: 'Click Me',
+//   color: 'primary',
+//   size: 'md',
+//   shape: 'default',
+//   textSize: 'md',
+//   outline: 'none',
+// }
