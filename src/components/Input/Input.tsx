3ccd4bec8a4a48b7210005fb372c382fd68f9cf8
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-28 23:55:20 +0900

    split code for input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,17 +1,6 @@
 import React from 'react'
-
-export type InputProps = {
-  placeholder?: string
-  borderColor?: 'default' | 'primary' | 'success' | 'danger'
-}
-
-export const getBorderColor = (colorKey: InputProps['borderColor']) =>
-  new Map<InputProps['borderColor'], string>([
-    ['default', 'border-gray border-2'],
-    ['primary', 'text-white bg-primary hover:bg-primary_hover'],
-    ['danger', 'text-white bg-danger hover:bg-danger_hover'],
-    ['success', 'text-white bg-success hover:bg-success_hover'],
-  ]).get(colorKey)
+import { getBorderColor } from './Input.styles'
+import { InputProps } from './Input.types'
 
 const Input = ({placeholder, borderColor}: InputProps) => {
   const colorStyle = getBorderColor(borderColor)
