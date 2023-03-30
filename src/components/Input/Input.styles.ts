332b8d79d8c547dbaa05494a199d7276d2ba0e22
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-30 14:21:26 +0900

    fix how to display input

--- src/components/Input/Input.styles.ts
+++ src/components/Input/Input.styles.ts
@@ -7,7 +7,7 @@ export const getBorderColor = (colorKey: InputProps['borderColor']) =>
 
 export const getWidth = (widthKey: InputProps['width']) =>
   new Map<InputProps['width'], string>([
-    ['sm', 'w-32 px-3 py-2 block'],
-    ['md', 'w-60 px-4 py-3 block'],
-    ['lg', 'w-full px-5 py-4 block'],
+    ['sm', 'w-32 px-3 block'],
+    ['md', 'w-60 px-4 block'],
+    ['lg', 'w-full px-5 block'],
   ]).get(widthKey)
